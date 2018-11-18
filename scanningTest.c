b6ec8779ad9c7044d5f0bdeb56a284070818b606
Abhinav <AbhinavA10@users.noreply.github.com>
2018-11-17 19:37:21 -0500

    added stuff

--- scanningTest.c
+++ scanningTest.c
@@ -1,8 +1,7 @@
-const float DIST_CONV=180/(2.75*PI);
-
-const int AXIAL = motorB;
+const int BACK_WHEELS = motorB;
 const int SWEEPER = motorA;
 const int COLOR_SENS=S2;
+const float WHEEL_RADIUS = 2.75
 
 const float WIDTH = 1.5; // x distance between each pixel
 const float HEIGHT = 0.75;
@@ -72,38 +71,49 @@ bool Digit6[9][5] = {
 						{0, 0, 0, 0, 0},
 						{0, 0, 0, 0, 0}	};
 
-float getSweepDistance(int angle){
-	// after modelling the data as
-	return (0.2995578422*angle+ 0.844450546);
+float getEncCountFromWheelDist(float distance){
+	return distance * (180.0/(WHEEL_RADIUS*PI));
 }
+float getWheelDistFromEncCount(double count){
+	return count / (180.0/(WHEEL_RADIUS*PI));
+}
+
 
 void nextPixelRight(){
 	motor[SWEEPER]=70;
 	nMotorEncoder[SWEEPER]=0;
-	// range of angle thing 
-	//goes from 0.844 to 1.5
-	// each 5 degrees moves it 1.5cm
-	while(getSweepDistance(nMotorEncoder[SWEEPER]) < 1.5+0.844){}
+	// insert function too convert wheel rope to linear distance
 	motor[SWEEPER]=0;
 }
 void nextPixelLeft(){
 	motor[SWEEPER]=-70;
 	nMotorEncoder[SWEEPER]=0;
-	// range of angle thing 
+	// range of angle thing
 	//goes from 0.844 to 1.5
 	while(getSweepDistance(nMotorEncoder[SWEEPER]) > 0.844-1.5){}
 	motor[SWEEPER]=0;
 }
-void nextLine(){
-	motor[AXIAL]=20;
-	nMotorEncoder[AXIAL]=0;
-	while(nMotorEncoder[AXIAL]/DIST_CONV < HEIGHT){}
-	motor[SWEEPER]=0;
+void moveRobotDown(int distanceDown, bool down){
+	nMotorEncoder[BACK_WHEELS] = 0;
+	if(down){
+		motor[BACK_WHEELS] = 10;
+		// moves down for specified distance
+		while(nMotorEncoder[BACK_WHEELS]<=(getEncCountFromWheelDist(distanceDown))){}
+		motor[BACK_WHEELS] = 0;
+	}
+		// moving up
+	else{
+		motor[BACK_WHEELS] = -10;
+		// moves down for specified distance
+		while(nMotorEncoder[BACK_WHEELS]>=((getEncCountFromWheelDist(distanceDown)))*-1){}
+		motor[BACK_WHEELS] = 0;
+	}
 }
+
 void goLeft(){
 	motor[SWEEPER]=-50;
-	nMotorEncoder[AXIAL]=0;
-	while(nMotorEncoder[AXIAL] > (-1)*ANGLE_SPAN){}
+	nMotorEncoder[SWEEPER]=0;
+	while(nMotorEncoder[SWEEPER] > (-1)*ANGLE_SPAN){}
 	motor[SWEEPER]=0;
 }
 void outputMatrix(){
@@ -147,12 +157,12 @@ task main(){
 					Digit6[row][column-10]=isBlack;
 				}
 			}
-			nextPixelRight(); // horizontal
+			//nextPixelRight(); // horizontal
 
-			//nextPixelLeft(); // horizontal
+			nextPixelLeft(); // horizontal
 			wait1Msec(1000);
 		}
-		nextLine();
+		moveRobotDown(HEIGHT, 1);
 		wait1Msec(1000);
 	}
 	outputMatrix();
